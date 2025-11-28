ea46ee6df58c1f312f966a14cdb0cf3b7409ba17
RataRabiosa <114504748+RataRabiosa@users.noreply.github.com>
2025-11-28 09:49:59 -0300

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -22,7 +22,8 @@ const Products = () => {
       try {
         // Esperar 2 segundos para simular carga
         await new Promise((resolve) => setTimeout(resolve, 2000));
-        const resp = await fetch("http://localhost:8000/products");
+        //const resp = await fetch("http://localhost:8000/products");
+        const resp = await fetch("https://restful-api-vercel-ashy.vercel.app/products");
         if (!resp.ok) {
           throw new Error(`Error al obtener productos - status: ${resp.status}`);
         }
